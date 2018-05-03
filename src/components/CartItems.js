12f7e561919a89b4556576249f2eff0cf92b33e8
Michael Herring <michael@michaelherring.net>
2018-05-02 18:57:21 -0600

    unbork by replacing 'cartItems' with 'cartItemsList'

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CartItems = props => {
 
-  let cartItemList = props.cartItems.map(
+  let cartItemList = props.cartItemsList.map(
     (items, i) => {
 
     return (
